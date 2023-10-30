1e950a58be48fdb09fd6e4ba3eecdeabdc87243c
kelvin-Linux <kelvinabache12@gmail.com>
2023-10-30 18:33:06 -0400

    primeras anotaciones

--- hello.sh
+++ hello.sh
@@ -1,1 +1,12 @@
-echo 'hello word'
+#! /bin/sh
+
+:'este cmando crea un archivo y agrega vas contenido :> este es uncomentario amplio
+echo hello word2 >> hello.txt'
+
+#aqui inicia el codigo
+cat << myText
+este una prueba 
+escrita en bash
+myText 
+
+#Aqui termina el codigo'
